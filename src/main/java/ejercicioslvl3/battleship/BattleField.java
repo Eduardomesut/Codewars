92d5b152cf0c7df658aa9e4e57f8e11e80d72111
eduardomesut <eduardomesut@gmail.com>
2024-12-17 13:26:33 +0100

    Seguir battleship

--- src/main/java/ejercicioslvl3/battleship/BattleField.java
+++ src/main/java/ejercicioslvl3/battleship/BattleField.java
@@ -7,11 +7,72 @@ public class BattleField {
 
 
 
+        for (int filas = 0; filas < field.length; filas++) {
+            for (int columnas = 0; columnas < field[filas].length; columnas++) {
+
+                if (field[filas][columnas] == 1){
+                    if (filas == 0 && columnas == 0){
+
+
+                    } else if (filas == 0 && columnas == field[filas].length - 1) {
+
+                    } else if (filas == field.length - 1 && columnas == 0) {
+
+                    } else if (filas == field.length - 1 && columnas == field[filas].length - 1) {
+
+                    } else if (filas == field.length - 1){
+                        if (field[filas][columnas + 1] == 1){
+                            if (field[filas + 1][columnas + 1] == 1){
+                                return false;
+                            }
+                            if (field[filas - 1][columnas + 1] == 1){
+                                return false;
+                            }
+
+                        }
+                        if (field[filas][columnas - 1] == 1){
+
+                        }
+
+                    }else if (columnas == field[filas].length - 1) {
+                        //Limites
+
+
+                    }else{
+                        if (field[filas][columnas + 1] == 1){
+
+                            if (field[filas][columnas + 1] == 1){
+                                if (field[filas + 1][columnas + 1] == 1){
+                                    return false;
+                                }
+                                if (field[filas - 1][columnas + 1] == 1){
+                                    return false;
+                                }
+                            }
+                        }
+                        if (field[filas][columnas - 1] == 1){
+
+                        }
+                    }
+
+
+
+                }
+
+                System.out.print(field[filas][columnas] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
         return false;
     }
 
     public static void main(String[] args) {
-        int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+        int[][] battleField =
+                {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
                 {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
                 {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
                 {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
