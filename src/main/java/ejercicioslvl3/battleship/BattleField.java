082db96b22da6b09f04dd4d6b102da89035d8662
eduardomesut <eduardomesut@gmail.com>
2024-12-17 02:54:22 +0100

    Empezar battleship

---
+++ src/main/java/ejercicioslvl3/battleship/BattleField.java
@@ -0,0 +1,28 @@
+package ejercicioslvl3.battleship;
+
+public class BattleField {
+
+    public static boolean fieldValidator(int[][] field) {
+        // your code here!
+
+
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] battleField = {{1, 0, 0, 0, 0, 1, 1, 0, 0, 0},
+                {1, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 1, 1, 0, 1, 0},
+                {1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+        System.out.println(fieldValidator(battleField));
+
+    }
+}
