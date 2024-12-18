5b29d8627614c90895e54bb8373678ebc6154263
eduardomer <eduardomesut@gmail.com>
2024-12-18 18:36:55 +0100

    Empezando Papers Please

---
+++ src/main/java/ejercicioslvl3/papersPlease/Inspector.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicioslvl3.papersPlease;
+
+/**
+ *
+ * @author tarde
+ */
+import java.util.*;
+
+public class Inspector {
+    
+    public void receiveBulletin(String bulletin) { 
+        // Your code here
+    }
+
+    public String inspect(Map<String, String> person) {
+        // Your code here
+        return "";
+    }
+    
+}
