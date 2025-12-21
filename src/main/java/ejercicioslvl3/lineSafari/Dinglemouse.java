2788373d03efac1499d3d885cdd2fa59e501598a
Eduardomesut <eduardomesut@gmail.com>
2025-12-21 13:25:53 +0100

    Update context and basics validators

--- src/main/java/ejercicioslvl3/lineSafari/Dinglemouse.java
+++ src/main/java/ejercicioslvl3/lineSafari/Dinglemouse.java
@@ -2,9 +2,50 @@ package ejercicioslvl3.lineSafari;
 
 public class Dinglemouse {
 
+    /* VALIDO
+    "                    ",
+    "     +--------+     ",
+    "  X--+        +--+  ",
+    "                 |  ",
+    "                 X  ",
+    "
+     */
+
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', '|', ' ', ' '},
+                {' ', ' ', '|', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '}
+        };
+        Dinglemouse.line(grid);
+
+
+    }
+
+
+
     public static boolean line(final char [][] grid) {
+        boolean horizontal;
+        boolean vertical;
+        //Hacer validaciones simples y luego seguir con las complejas
+        //Intentar hacer con programación funcional
+        horizontal = validateHorizontal(grid);
+        vertical = validateVertical(grid);
         // Your code here
+        System.out.println(grid.length + " " + grid[0].length);
         return false;
     }
+
+    private static boolean validateHorizontal(final char [][] grid) {
+        return false;
+    }
+    private static boolean validateVertical(final char [][] grid) {
+        return false;
+    }
+
+
 
 }
