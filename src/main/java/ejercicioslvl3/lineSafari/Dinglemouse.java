f71982ed3fced9fe0cbd433c2ca510f25e6f1e2d
Eduardomesut <eduardomesut@gmail.com>
2025-12-21 01:44:11 +0100

    Base model

---
+++ src/main/java/ejercicioslvl3/lineSafari/Dinglemouse.java
@@ -0,0 +1,10 @@
+package ejercicioslvl3.lineSafari;
+
+public class Dinglemouse {
+
+    public static boolean line(final char [][] grid) {
+        // Your code here
+        return false;
+    }
+
+}
